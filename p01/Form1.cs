d935f87c3f3549d1e0be0a63a9f2af5a68e679bd
Ugurt 8,90 <keresztesp.07@spst.eu>
2025-09-18 13:14:53 +0200

    PRG
    KOOKOT

---
+++ p01/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string samohlasky = "aeiouyáéěíóúůý";
+            string souhlasky = "bcčdďfghjklmnpqrřsštťvwxzž";
+            string veta=textBox1.Text;
+            int pocetsam = 0;
+            int pocetsouh = 0;
+            int nepismene = 0;
+            veta = veta.ToLower();
+            foreach(char c in veta)
+            {
+                if (samohlasky.Contains(c))
+                {
+                    pocetsam++;
+                }
+                else
+                {
+                    if (souhlasky.Contains(c))
+                    {
+                        pocetsouh++;
+                    }
+                    else
+                    {
+                        nepismene++;
+                    }
+                }
+            }
+            MessageBox.Show("Samohlasek je " + pocetsam + " a souhlasek je " + pocetsouh + " a nepismenych znaku je " + nepismene);
+        }
+    }
+}
---
+++ p02/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sifra = textBox1.Text;
+            int kolik=Convert.ToInt32(textBox2.Text);
+            string zasifrovane = string.Empty;
+       
+            foreach(char c in sifra)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    if (c+(char)kolik>'Z')
+                    {
+                        zasifrovane += (char)('A' + (kolik-1))%26;
+                    }
+                    else
+                    {
+                        zasifrovane += (char)(c + (kolik));
+                    }
+                    
+                }
+                else
+                {
+                    if (c >= 'a' && c <= 'z')
+                    {
+                        if (c + (char)kolik > 'z')
+                        {
+                            zasifrovane += (char)('a' + (kolik-1) )%26;
+                        }
+                        else
+                        {
+                            zasifrovane += (char)(c + (kolik));
+                        }
+                    }
+                }
+
+                
+            }
+            MessageBox.Show("Zasifrovane je " + zasifrovane);
+            label2.Text = zasifrovane;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string zasifrovane = label2.Text;
+            int kolik = Convert.ToInt32(textBox2.Text);
+            string odsifrovane = string.Empty;
+            foreach(char c in zasifrovane)
+            {
+                if (c >= 'A' && c <= 'Z')
+                {
+                    if (c - (char)kolik > 'Z')
+                    {
+                        odsifrovane += (char)('Z' - (kolik) - 1);
+                    }
+                    else
+                    {
+                        odsifrovane += (char)(c - (kolik));
+                    }
+
+                }
+                else
+                {
+                    if (c >= 'a' && c <= 'z')
+                    {
+                        if (c - (char)kolik > 'z')
+                        {
+                            odsifrovane += (char)('z' - (kolik) - 1);
+                        }
+                        else
+                        {
+                            odsifrovane += (char)(c - (kolik));
+                        }
+                    }
+                }
+            }
+            MessageBox.Show("Zasifrovane je " + odsifrovane);
+        }
+    }
+}
